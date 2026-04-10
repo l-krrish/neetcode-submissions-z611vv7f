623027fba16a043f542c4e5267f5b58869f559f7
Krrish Lala <k2lala@uwaterloo.ca>
2026-04-10 09:44:42 -0400

    Add: binary-tree-right-side-view - submission-1

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-1.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        level_map={}
+        def dfs(node,depth):
+            if not node:
+                return 
+            level_map[depth]=node.val
+            dfs(node.left,depth+1)
+            dfs(node.right,depth+1)
+        dfs(root,0)
+        return [level_map[i] for i in range(len(level_map))]
